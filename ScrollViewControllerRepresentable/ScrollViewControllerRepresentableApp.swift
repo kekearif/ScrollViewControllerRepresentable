27c1db45b1857eca67e95f31d6cef453db2fed1e
Keke <kekearif@googlemail.com>
2023-02-09 14:26:49 +0800

    Initial Commit

---
+++ ScrollViewControllerRepresentable/ScrollViewControllerRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewControllerRepresentableApp.swift
+//  ScrollViewControllerRepresentable
+//
+//  Created by Keke Arif on 2023/2/9.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewControllerRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
